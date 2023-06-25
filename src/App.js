eae26a90a9a1288c24b1c4508eceba347b88f594
WNagorny <walery.nagorny@gmail.com>
2023-06-25 17:41:14 +0200

    added top section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import './App.css';
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-md">
+        <TopButtons/>
+        <Inputs/>
     </div>
   );
 }
